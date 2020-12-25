fa0d8836a7525a924bbea8d7cbe55a81e205eea2
Murilo Sanches <murilando@gmail.com>
2020-12-24 21:16:16 -0300

    FirstCommit

---
+++ spotify/tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        '14' : '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D4689',
+        'green-main': '#1ED760',
+      },
+      
+      backgroundImage: theme => ({
+        'spotify-theme':"url('../img/bursts.svg')",
+        'spotify-theme-mobile':"url('../img/bursts-mobile.svg')",
+      }),
+
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center',
+      },
+
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
